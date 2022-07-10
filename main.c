8d04c477411ea79c3aa25b6ac5cbeba635e32312
hatto <ryosuke.10117@gmail.com>
2022-07-10 23:40:01 +0900

    :sparkles: add mul, div and brackets

--- main.c
+++ main.c
@@ -25,7 +25,22 @@ Token *token;
 void error(char *fmt, ...){
     va_list ap;
     va_start(ap, fmt);
-    vprintf(fmt, ap);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+char *user_input;
+
+void error_at(char *loc, char *fmt, ...){
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " ");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
 }
@@ -77,7 +92,7 @@ Token* tokenize(char *p){
             continue;
         }
 
-        if(*p == '+' || *p == '-'){
+        if(strchr("+-*/()", *p)){
             cur = new_token(TK_RESERVED, cur, p++);
             continue;
         }
@@ -88,11 +103,117 @@ Token* tokenize(char *p){
             continue;
         }
 
-        error("disable to tokenize");
+        error_at(p, "disable to tokenize");
     }
     
     new_token(TK_EOF, cur, p);
     return head.next;
+}
+
+typedef enum{
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM
+} NodeKind;
+
+typedef struct Node Node;
+struct Node{
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val;
+};
+
+Node* new_node(NodeKind kind){
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    return node;
+}
+
+Node* new_binary(NodeKind kind, Node *lhs, Node *rhs){
+    Node *node = new_node(kind);
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_num(int val){
+    Node *node = new_node(ND_NUM);
+    node->val = val;
+    return node;
+}
+
+Node* expr();
+Node* mul();
+Node* primary();
+
+Node* expr(){
+    Node *node = mul();
+
+    for(;;){
+        if(consume('+')){
+            node = new_binary(ND_ADD, node, mul());
+        }else if(consume('-')){
+            node = new_binary(ND_SUB, node, mul());
+        }else{
+            return node;
+        }
+    }
+}
+
+Node* mul(){
+    Node *node = primary();
+
+    for(;;){
+        if(consume('*')){
+            node = new_binary(ND_MUL, node, primary());
+        }else if(consume('/')){
+            node = new_binary(ND_DIV, node, primary());
+        }else{
+            return node;
+        }
+    }
+}
+
+Node* primary(){
+    if(consume('(')){
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+    return new_num(expect_number());
+}
+
+void gen(Node *node){
+    if(node->kind == ND_NUM){
+        printf("\tpush %d\n", node->val);
+        return;
+    }
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("\tpop rdi\n");
+    printf("\tpop rax\n");
+
+    switch (node->kind)
+    {
+    case ND_ADD:
+        printf("\tadd rax, rdi\n");
+        break;
+    case ND_SUB:
+        printf("\tsub rax, rdi\n");
+        break;
+    case ND_MUL:
+        printf("\timul rax, rdi\n");
+        break;
+    case ND_DIV:
+        printf("\tcqo\n");
+        printf("\tidiv rdi\n");
+        break;
+    }  
+    printf("\tpush rax\n");
 }
 
 int main(int argc, char **argv){
@@ -101,22 +222,17 @@ int main(int argc, char **argv){
         return 1;
     }
 
+    user_input = argv[1];
+
     token = tokenize(argv[1]);
+    Node *node = expr();
 
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
-    printf("main:");
-    printf("\tmov rax, %d\n", expect_number());
+    printf("main:\n");
 
-    while(!at_eof()){
-        if(consume('+')){
-            printf("\tadd rax, %d\n", expect_number());
-            continue;
-        }
-
-        expect('-');
-        printf("\tsub rax, %d\n", expect_number());
-    }
+    gen(node);
+    printf("\tpop rax\n");
         
     printf("\tret\n");
     return 0;
--- test.sh
+++ test.sh
@@ -18,5 +18,11 @@ assert(){
 
 assert 0 0 
 assert 42 42
+assert 21 "5+20-4"
+assert 21 " 5 + 20 - 4 "
+assert 14 "2+3*4"
+assert 4  "2*(6 - 4)"
+assert 10 "((4 + 6) / 2) * (3 - 1)"
 
-echo OK
+
+echo OK 
